12b59661951b79491b762309353a45f6dd2d3f4f
zadiss <78500140+zadiss@users.noreply.github.com>
2021-02-03 19:06:03 +0000

    Deploying to gh-pages from master @ 299df6c12dad02b0c01ee0c6f89d390bd9f599cf 🚀

---
+++ tilesets/[Base]BaseChip_pipo.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="../../../../Downloads/tileset/Pipoya RPG Tileset 32x32/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+ <tile id="73">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="1000"/>
+   <frame tileid="2" duration="1000"/>
+   <frame tileid="3" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
