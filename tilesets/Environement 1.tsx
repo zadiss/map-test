1c09ae1f79a71f81e814546554d1d334d789279e
zadiss <78500140+zadiss@users.noreply.github.com>
2021-02-04 00:36:50 +0000

    Deploying to gh-pages from master @ aa23ddf9f9c38330ca87fb19b7ce4d4c57859567 🚀

--- tilesets/Environement 1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Environement 1" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="../../../../Downloads/tileset/Environement 1.png" width="512" height="384"/>
-</tileset>
